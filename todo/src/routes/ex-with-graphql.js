60b2e7fd303724d080805e3dff8c56a4cc037e2b
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2018-01-16 22:51:44 +0530

    Calling GraphQL APis for creating and fetching todos

--- todo/src/routes/ex-with-graphql.js
+++ todo/src/routes/ex-with-graphql.js
@@ -2,4 +2,10 @@ const router = require('express').Router();
 
 module.exports = router;
 
-router.get('/', (req, res, next) => res.render('ex-with-graphql/index'));
+router.get('/', (req, res, next) => res.render('ex-with-graphql/index', {
+    incomplete: true,
+}));
+
+router.get('/completed', (req, res, next) => res.render('ex-with-graphql/index', {
+    complete: true,
+}));
